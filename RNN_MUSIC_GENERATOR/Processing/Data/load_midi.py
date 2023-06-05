5a33f1b9e1a74d28d1635cf028a1bb81670ceaac
Cristian Lagranda <cristianlagranda@gmail.com>
2023-06-05 16:14:37 +0200

    Created Load_Songs and encoded_files functions

---
+++ RNN_MUSIC_GENERATOR/Processing/Data/load_midi.py
@@ -0,0 +1,40 @@
+import music21 as m21
+
+def encode_song(song, time_step=0.25):
+    """Converts a score into a time-series-like music representation. Each item in the encoded list represents 'min_duration'
+    quarter lengths. The symbols used at each step are: integers for MIDI notes, 'r' for representing a rest, and '_'
+    for representing notes/rests that are carried over into a new time step. Here's a sample encoding:
+
+        ["r", "_", "60", "_", "_", "_", "72" "_"]
+
+    :param song (m21 stream): Piece to encode
+    :param time_step (float): Duration of each time step in quarter length
+    :return:
+    """
+
+    encoded_song = []
+
+    for event in song.flat.notesAndRests:
+
+        # handle notes
+        if isinstance(event, m21.note.Note):
+            symbol = event.pitch.midi # 60
+        # handle rests
+        elif isinstance(event, m21.note.Rest):
+            symbol = "r"
+
+        # convert the note/rest into time series notation
+        steps = int(event.duration.quarterLength / time_step)
+        for step in range(steps):
+
+            # if it's the first time we see a note/rest, let's encode it. Otherwise, it means we're carrying the same
+            # symbol in a new time step
+            if step == 0:
+                encoded_song.append(symbol)
+            else:
+                encoded_song.append("_")
+
+    # cast encoded song to str
+    encoded_song = " ".join(map(str, encoded_song))
+
+    return encoded_song
---
+++ RNN_MUSIC_GENERATOR/Processing/processing.py
@@ -0,0 +1,48 @@
+import os
+import music21 as m21
+from Data.load_midi import encode_song
+
+'''
+MIDI FILES GIVES ME THE FOLLOWING INFORMATION:
+NOTA:C4 , DURATION: 0.25s, VELOCIDAD: 125
+SILENCIO: , DURATION:
+'''
+DATASET_PATH = '/Users/Cris/code/miguimorell/RNN_MUSIC_GENERATOR/raw_data'
+ENCODED_PATH = '/Users/Cris/code/miguimorell/RNN_MUSIC_GENERATOR/RNN_MUSIC_GENERATOR/Processing/Data/encoded'
+
+def load_songs(dataset_path):
+    """Loads all kern pieces in dataset using music21.
+
+    :param dataset_path (str): Path to dataset
+    :return songs (list of m21 streams): List containing all pieces
+    """
+    songs = []
+    files_name = []
+
+    # go through all the files in dataset and load them with music21
+    for path, _, files in os.walk(dataset_path):
+        for file in files:
+            if file[-3:] == "mid":
+                song = m21.converter.parse(os.path.join(path, file))
+                songs.append(song)
+                files_name.append(file[-9:-4])
+    return songs,files_name
+
+def create_encoded_files(songs,files_name):
+    for song,file_name in zip(songs,files_name):
+        encoded_song=encode_song(song)
+        # save songs to text file
+        save_path = os.path.join(ENCODED_PATH, file_name)
+        with open(save_path, "w") as fp:
+            fp.write(encoded_song)
+
+
+def main():
+
+    songs,files_name = load_songs(DATASET_PATH)
+
+    #generate encoded files
+    create_encoded_files(songs,files_name)
+
+if __name__ == "__main__":
+    main()
